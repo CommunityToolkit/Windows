f1b8fd364db31d4bedd3d227c07bc62b79a9c156
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-04-05 15:27:59 -0700

    Shore up DispatcherQueueTimer Debounce test

--- components/Extensions/tests/DispatcherQueueTimerExtensionTests.cs
+++ components/Extensions/tests/DispatcherQueueTimerExtensionTests.cs
@@ -40,6 +40,10 @@ public partial class DispatcherQueueTimerExtensionTests : VisualUITestBase
             },
             TimeSpan.FromMilliseconds(60));
 
+        Assert.AreEqual(true, debounceTimer.IsRunning, "Expected time to be running.");
+        Assert.AreEqual(0, triggeredCount, "Function shouldn't have run yet.");
+        Assert.IsNull(triggeredValue, "Function shouldn't have run yet.");
+
         var value2 = "Hello";
         debounceTimer.Debounce(
             () =>
@@ -49,10 +53,52 @@ public partial class DispatcherQueueTimerExtensionTests : VisualUITestBase
             },
             TimeSpan.FromMilliseconds(60));
 
+        Assert.AreEqual(true, debounceTimer.IsRunning, "Expected time to be running.");
+
         await Task.Delay(TimeSpan.FromMilliseconds(110));
 
         Assert.AreEqual(false, debounceTimer.IsRunning, "Expected to stop the timer.");
         Assert.AreEqual(value2, triggeredValue, "Expected to execute the last action.");
         Assert.AreEqual(1, triggeredCount, "Expected to postpone execution.");
+    }
+
+    [TestCategory("DispatcherQueueTimerExtensions")]
+    [UIThreadTestMethod]
+    public async Task DispatcherQueueTimer_Debounce_Immediate_Interrupt()
+    {
+        var debounceTimer = DispatcherQueue.GetForCurrentThread().CreateTimer();
+
+        var triggeredCount = 0;
+        string? triggeredValue = null;
+
+        var value = "He";
+        debounceTimer.Debounce(
+            () =>
+            {
+                triggeredCount++;
+                triggeredValue = value;
+            },
+            TimeSpan.FromMilliseconds(60), true);
+
+        Assert.AreEqual(true, debounceTimer.IsRunning, "Expected time to be running.");
+        Assert.AreEqual(1, triggeredCount, "Function should have run right away.");
+        Assert.AreEqual(value, triggeredValue, "Should have expected immediate set of value");
+
+        var value2 = "Hello";
+        debounceTimer.Debounce(
+            () =>
+            {
+                triggeredCount++;
+                triggeredValue = value2;
+            },
+            TimeSpan.FromMilliseconds(60));
+
+        Assert.AreEqual(true, debounceTimer.IsRunning, "Expected time to be running.");
+
+        await Task.Delay(TimeSpan.FromMilliseconds(110));
+
+        Assert.AreEqual(false, debounceTimer.IsRunning, "Expected to stop the timer.");
+        Assert.AreEqual(value2, triggeredValue, "Expected to execute the last action.");
+        Assert.AreEqual(2, triggeredCount, "Expected to postpone execution.");
     }
 }
