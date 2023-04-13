09f61d896e6185b5bd980cbe98a9ebe941fe39cc
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-04-12 23:14:19 -0700

    Initial Raw Port into New Experiment

---
+++ components/Effects/tests/ExampleEffectsTestClass.cs
@@ -0,0 +1,133 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Tooling.TestGen;
+using CommunityToolkit.Tests;
+using CommunityToolkit.WinUI.Controls;
+
+namespace EffectsExperiment.Tests;
+
+[TestClass]
+public partial class ExampleEffectsTestClass : VisualUITestBase
+{
+    // If you don't need access to UI objects directly or async code, use this pattern.
+    [TestMethod]
+    public void SimpleSynchronousExampleTest()
+    {
+        var assembly = typeof(Effects).Assembly;
+        var type = assembly.GetType(typeof(Effects).FullName ?? string.Empty);
+
+        Assert.IsNotNull(type, "Could not find Effects type.");
+        Assert.AreEqual(typeof(Effects), type, "Type of Effects does not match expected type.");
+    }
+
+    // If you don't need access to UI objects directly, use this pattern.
+    [TestMethod]
+    public async Task SimpleAsyncExampleTest()
+    {
+        await Task.Delay(250);
+
+        Assert.IsTrue(true);
+    }
+
+    // Example that shows how to check for exception throwing.
+    [TestMethod]
+    public void SimpleExceptionCheckTest()
+    {
+        // If you need to check exceptions occur for invalid inputs, etc...
+        // Use Assert.ThrowsException to limit the scope to where you expect the error to occur.
+        // Otherwise, using the ExpectedException attribute could swallow or
+        // catch other issues in setup code.
+        Assert.ThrowsException<NotImplementedException>(() => throw new NotImplementedException());
+    }
+
+    // The UIThreadTestMethod automatically dispatches to the UI for us to work with UI objects.
+    [UIThreadTestMethod]
+    public void SimpleUIAttributeExampleTest()
+    {
+        var component = new Effects();
+        Assert.IsNotNull(component);
+    }
+
+    // The UIThreadTestMethod can also easily grab a XAML Page for us by passing its type as a parameter.
+    // This lets us actually test a control as it would behave within an actual application.
+    // The page will already be loaded by the time your test is called.
+    [UIThreadTestMethod]
+    public void SimpleUIExamplePageTest(ExampleEffectsTestPage page)
+    {
+        // You can use the Toolkit Visual Tree helpers here to find the component by type or name:
+        var component = page.FindDescendant<Effects_ClassicBinding>();
+
+        Assert.IsNotNull(component);
+
+        var componentByName = page.FindDescendant("EffectsControl");
+
+        Assert.IsNotNull(componentByName);
+    }
+
+    // You can still do async work with a UIThreadTestMethod as well.
+    [UIThreadTestMethod]
+    public async Task SimpleAsyncUIExamplePageTest(ExampleEffectsTestPage page)
+    {
+        // This helper can be used to wait for a rendering pass to complete.
+        await CompositionTargetHelper.ExecuteAfterCompositionRenderingAsync(() => { });
+
+        var component = page.FindDescendant<Effects_ClassicBinding>();
+
+        Assert.IsNotNull(component);
+    }
+
+    //// ----------------------------- ADVANCED TEST SCENARIOS -----------------------------
+
+    // If you need to use DataRow, you can use this pattern with the UI dispatch still.
+    // Otherwise, checkout the UIThreadTestMethod attribute above.
+    // See https://github.com/CommunityToolkit/Labs-Windows/issues/186
+    [TestMethod]
+    public async Task ComplexAsyncUIExampleTest()
+    {
+        await EnqueueAsync(() =>
+        {
+            var component = new Effects_ClassicBinding();
+            Assert.IsNotNull(component);
+        });
+    }
+
+    // If you want to load other content not within a XAML page using the UIThreadTestMethod above.
+    // Then you can do that using the Load/UnloadTestContentAsync methods.
+    [TestMethod]
+    public async Task ComplexAsyncLoadUIExampleTest()
+    {
+        await EnqueueAsync(async () =>
+        {
+            var component = new Effects_ClassicBinding();
+            Assert.IsNotNull(component);
+            Assert.IsFalse(component.IsLoaded);
+
+            await LoadTestContentAsync(component);
+
+            Assert.IsTrue(component.IsLoaded);
+
+            await UnloadTestContentAsync(component);
+
+            Assert.IsFalse(component.IsLoaded);
+        });
+    }
+
+    // You can still use the UIThreadTestMethod to remove the extra layer for the dispatcher as well:
+    [UIThreadTestMethod]
+    public async Task ComplexAsyncLoadUIExampleWithoutDispatcherTest()
+    {
+        var component = new Effects_ClassicBinding();
+        Assert.IsNotNull(component);
+        Assert.IsFalse(component.IsLoaded);
+
+        await LoadTestContentAsync(component);
+
+        Assert.IsTrue(component.IsLoaded);
+
+        await UnloadTestContentAsync(component);
+
+        Assert.IsFalse(component.IsLoaded);
+    }
+}
