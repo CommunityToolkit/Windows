2c55eec05aafe33cced42c64ccc606a5b3e67d03
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-04-12 23:14:19 -0700

    Initial Raw Port into New Experiment

---
+++ components/Effects/samples/EffectsTemplatedSample.xaml.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace EffectsExperiment.Samples;
+
+[ToolkitSampleBoolOption("IsTextVisible", true, Title = "IsVisible")]
+// Single values without a colon are used for both label and value.
+// To provide a different label for the value, separate with a colon surrounded by a single space on both sides ("label : value").
+[ToolkitSampleNumericOption("TextSize", 12, 8, 48, 2, false, Title = "FontSize")]
+[ToolkitSampleMultiChoiceOption("TextFontFamily", "Segoe UI", "Arial", "Consolas", Title = "Font family")]
+[ToolkitSampleMultiChoiceOption("TextForeground", "Teal : #0ddc8c", "Sand : #e7a676", "Dull green : #5d7577", Title = "Text foreground")]
+
+[ToolkitSample(id: nameof(EffectsTemplatedSample), "Templated control", description: "A sample for showing how to create and use a templated control.")]
+public sealed partial class EffectsTemplatedSample : Page
+{
+    public EffectsTemplatedSample()
+    {
+        this.InitializeComponent();
+    }
+}
