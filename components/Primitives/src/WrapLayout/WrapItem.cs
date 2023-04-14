e1608e34b9c96de9bc872dfaf0e6add12164a02f
Niels Laute <niels.laute@live.nl>
2023-04-14 12:08:57 +0200

    Porting primitives

---
+++ components/Primitives/src/WrapLayout/WrapItem.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CommunityToolkit.WinUI;
+
+internal class WrapItem
+{
+    public WrapItem(int index)
+    {
+        this.Index = index;
+    }
+
+    public int Index { get; }
+
+    public UvMeasure? Measure { get; internal set; }
+
+    public UvMeasure? Position { get; internal set; }
+
+    public UIElement? Element { get; internal set; }
+}
