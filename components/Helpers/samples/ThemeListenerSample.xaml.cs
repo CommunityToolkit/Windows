90e52f857e902a8683dd2b4f10bb1b5285b85287
Niels Laute <niels.laute@live.nl>
2023-05-02 16:50:22 +0200

    Adding NetworkHelper

--- components/Helpers/samples/ThemeListenerSample.xaml.cs
+++ components/Helpers/samples/ThemeListenerSample.xaml.cs
@@ -2,18 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using CommunityToolkit.WinUI;
 using CommunityToolkit.WinUI.Helpers;
-using Microsoft.UI;
-using Windows.UI;
 
 namespace HelpersExperiment.Samples;
-
-/// <summary>
-/// An example sample page of a custom control inheriting from Panel.
-/// </summary>
-//[ToolkitSampleTextOption("TitleText", "This is a title", Title = "Input the text")]
-//[ToolkitSampleMultiChoiceOption("LayoutOrientation", "Horizontal", "Vertical", Title = "Orientation")]
 
 [ToolkitSample(id: nameof(ThemeListenerSample), "Theme Listener", description: $"A sample for showing how to use {nameof(ThemeListener)}.")]
 public sealed partial class ThemeListenerSample : Page
@@ -25,7 +16,6 @@ public sealed partial class ThemeListenerSample : Page
         this.Loaded += ThemeListenerPage_Loaded;
         Listener.ThemeChanged += Listener_ThemeChanged;
         this.ActualThemeChanged += this.ThemeListenerSample_ActualThemeChanged;
-        float result = ScreenUnitHelper.Convert(ScreenUnit.Inch, ScreenUnit.Pixel, 1);
     }
 
     private void ThemeListenerSample_ActualThemeChanged(FrameworkElement sender, object args)
