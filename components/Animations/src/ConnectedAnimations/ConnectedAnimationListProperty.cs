69b2bc68d884f7bf10b18a59bda478889ec47dd3
Niels Laute <niels.laute@live.nl>
2023-05-11 17:00:36 +0200

    Init

---
+++ components/Animations/src/ConnectedAnimations/ConnectedAnimationListProperty.cs
@@ -0,0 +1,12 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CommunityToolkit.WinUI.Animations;
+
+internal class ConnectedAnimationListProperty
+{
+    public string ElementName { get; set; }
+
+    public Windows.UI.Xaml.Controls.ListViewBase ListViewBase { get; set; }
+}
---
+++ components/Animations/src/Xaml/Default/OpacityAnimation.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CommunityToolkit.WinUI.Animations;
+
+/// <summary>
+/// An opacity animation working on the composition or layer.
+/// </summary>
+public sealed class OpacityAnimation : ImplicitAnimation<double?, double>
+{
+    /// <inheritdoc/>
+    protected override string ExplicitTarget => nameof(Visual.Opacity);
+
+    /// <inheritdoc/>
+    protected override (double?, double?) GetParsedValues()
+    {
+        return (To, From);
+    }
+}
