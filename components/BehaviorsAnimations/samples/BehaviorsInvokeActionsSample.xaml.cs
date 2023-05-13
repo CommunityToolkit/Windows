0d7aac63183ce220c431bbb901d80548f3dbb27d
Niels Laute <niels.laute@live.nl>
2023-05-13 16:44:35 +0200

    Init

---
+++ components/BehaviorsAnimations/samples/BehaviorsInvokeActionsSample.xaml.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Animations;
+
+namespace Behaviors_AnimationsExperiment.Samples;
+
+[ToolkitSample(id: nameof(BehaviorsInvokeActionsSample), "InvokeActionsActivity", description: $"A sample for showing how to create and use a {nameof(InvokeActionsActivity)} behavior.")]
+public sealed partial class BehaviorsInvokeActionsSample : Page
+{
+    public BehaviorsInvokeActionsSample()
+    {
+        this.InitializeComponent();
+    }
+}
