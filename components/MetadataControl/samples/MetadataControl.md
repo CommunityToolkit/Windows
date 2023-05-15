3b4cb6ae400808fef32a9979d01ed68c1e367e9a
Niels Laute <niels.laute@live.nl>
2023-05-15 12:06:28 +0200

    Init

---
+++ components/MetadataControl/samples/MetadataControl.md
@@ -0,0 +1,60 @@
+---
+title: MetadataControl
+author: vgromfeld
+description: The MetadataControl control displays a list of labels and hyper-links separated by a bullet.
+keywords: MetadataControl, Control, metadata
+dev_langs:
+  - csharp
+category: Controls
+subcategory: Layout
+discussion-id: 0
+issue-id: 0
+---
+
+# MetadataControl
+
+The [MetadataControl](/dotnet/api/microsoft.toolkit.uwp.ui.controls.metadatacontrol) control displays a
+list of labels and hyper-links separated by a bullet.
+It also generates an accessible string representing its content.
+
+The bullet separator can be customized using the `Separator` property.
+`AccessibleSeparator` is used as a replacement for `Separator` to generate the accessible string.
+
+The control needs a list of [MediadataItem](https://github.com/windows-toolkit/WindowsCommunityToolkit/blob/main/Microsoft.Toolkit.Uwp.UI.Controls.Core/MetadataControl/MetadataItem.cs).
+Each item will be displayed either as a text or as an hyper-link (if the `Command`property is set).
+
+The default control template is using on a `TextBlock`. The style of this `TextBlock` can be customized using the `TextBlockStyle` property.
+
+> [!SAMPLE MetadataControlSample]
+
+## Example
+
+Add the control in the page:
+
+```xaml
+<controls:MetadataControl
+    x:Name="metadataControl"
+    Separator="   "
+    AccessibleSeparator=", "/>
+```
+
+Add items to control:
+
+```cs
+metadataControl.Items = new[]
+{
+    new MetadataItem { Label = "Hello" },
+    new MetadataItem { Label = "World", Command = myCommand },
+};
+```
+
+## MediadataItem
+
+A `MediadataItem` contains the information about one entry which will be displayed in the `MetadataControl`
+
+| Property | Type | Description |
+| -- | -- | -- |
+| Label | String | Gets or sets the label of the item |
+| AccessibleLabel | String | Gets or sets the automation name that will be set on the item. If not set, `Label` will be used. |
+| Command | ICommand | Gets or sets the command associated to the item. If null, the item will be displayed as a text field. If set, the item will be displayed as an hyperlink. |
+| CommandParameter | Object | Gets or sets the parameter that will be provided to the `Command`|
---
+++ components/MetadataControl/samples/MetadataControlSample.cs
@@ -0,0 +1,240 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Controls;
+using System.Windows.Input;
+
+namespace MetadataControlExperiment.Samples;
+
+/// <summary>
+/// An example sample page of a custom control inheriting from Panel.
+/// </summary>
+[ToolkitSampleTextOption("Separator", " • ", Title = "Separator")]
+[ToolkitSampleTextOption("AccessibleSeparator", " • ", Title = "AccessibleSeparator")]
+
+[ToolkitSample(id: nameof(MetadataControlSample), "MetadataControl", description: $"A sample for showing how to create and use a {nameof(MetadataControl)} control.")]
+public sealed partial class MetadataControlSample : Page
+{
+    private static readonly string[] Labels = "Lorem ipsum dolor sit amet consectetur adipiscing elit".Split(' ');
+
+    private readonly Random _random;
+    private readonly ObservableCollection<MetadataItem> _units;
+    private readonly DelegateCommand<object> _command;
+
+    public MetadataControlSample()
+    {
+        this.InitializeComponent();
+        _random = new Random();
+        _units = new ObservableCollection<MetadataItem>();
+        _command = new DelegateCommand<object>(OnExecuteCommand);
+        metadataControl.Items = _units;
+    }
+
+    private string GetRandomLabel() => Labels[_random!.Next(Labels.Length)];
+
+    private async void OnExecuteCommand(object obj)
+    {
+        var dialog = new ContentDialog
+        {
+            Title = "Command invoked",
+            Content = $"Command parameter: {obj}",
+            CloseButtonText = "OK"
+        };
+
+        await dialog.ShowAsync();
+    }
+
+    private void AddLabel_Click(object sender, RoutedEventArgs e)
+    {
+        if (_units != null)
+        {
+            _units.Add(new MetadataItem { Label = GetRandomLabel() });
+        }
+    }
+
+    private void AddCommand_Click(object sender, RoutedEventArgs e)
+    {
+        if (_units != null)
+        {
+            var label = GetRandomLabel();
+            _units.Add(new MetadataItem
+            {
+                Label = label,
+                Command = _command!,
+                CommandParameter = label,
+            });
+        }
+    }
+
+    private void Clear_Click(object sender, RoutedEventArgs e)
+    {
+        if (_units != null)
+        {
+            _units.Clear();
+        }
+    }
+}
+
+public class DelegateCommand : ICommand
+{
+    private readonly Action commandExecuteAction;
+
+    private readonly Func<bool> commandCanExecute;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+    /// </summary>
+    /// <param name="execute">
+    /// The action to execute when called.
+    /// </param>
+    /// <param name="canExecute">
+    /// The function to call to determine if the command can execute the action.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if the execute action is null.
+    /// </exception>
+    public DelegateCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        if (execute == null)
+        {
+            throw new ArgumentNullException(nameof(execute));
+        }
+
+        commandExecuteAction = execute;
+        commandCanExecute = canExecute ?? (() => true);
+    }
+
+    /// <summary>
+    /// Occurs when changes occur that affect whether or not the command should execute.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged;
+
+    /// <summary>
+    /// Defines the method that determines whether the command can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">
+    /// The parameter used by the command.
+    /// </param>
+    /// <returns>
+    /// Returns a value indicating whether this command can be executed.
+    /// </returns>
+    public bool CanExecute(object? parameter = null)
+    {
+        try
+        {
+            return commandCanExecute();
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Defines the method to be called when the command is invoked.
+    /// </summary>
+    /// <param name="parameter">
+    /// The parameter used by the command.
+    /// </param>
+    public void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter))
+        {
+            return;
+        }
+
+        try
+        {
+            commandExecuteAction();
+        }
+        catch
+        {
+            Debugger.Break();
+        }
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+
+public class DelegateCommand<T> : ICommand
+{
+    private readonly Action<T> commandExecuteAction;
+
+    private readonly Func<T, bool> commandCanExecute;
+
+    public DelegateCommand(Action<T> executeAction, Func<T, bool>? canExecute = null)
+    {
+        if (executeAction == null)
+        {
+            throw new ArgumentNullException(nameof(executeAction));
+        }
+
+        commandExecuteAction = executeAction;
+        commandCanExecute = canExecute ?? (e => true);
+    }
+
+    /// <summary>
+    /// Occurs when changes occur that affect whether or not the command should execute.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged;
+
+    /// <summary>
+    /// Defines the method that determines whether the command can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">
+    /// The parameter used by the command.
+    /// </param>
+    /// <returns>
+    /// Returns a value indicating whether this command can be executed.
+    /// </returns>
+    public bool CanExecute(object? parameter)
+    {
+        try
+        {
+            return commandCanExecute(ConvertParameterValue(parameter!));
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Defines the method to be called when the command is invoked.
+    /// </summary>
+    /// <param name="parameter">
+    /// The parameter used by the command.
+    /// </param>
+    public void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter))
+        {
+            return;
+        }
+
+        try
+        {
+            commandExecuteAction(ConvertParameterValue(parameter!));
+        }
+        catch
+        {
+            Debugger.Break();
+        }
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private static T ConvertParameterValue(object parameter)
+    {
+        parameter = parameter is T ? parameter : Convert.ChangeType(parameter, typeof(T));
+        return (T)parameter;
+    }
+}
