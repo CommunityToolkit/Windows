237f479965ea86a87648cb471139449d52b9633c
Niels Laute <niels.laute@live.nl>
2023-05-15 17:44:28 +0200

    Initial port

---
+++ components/HeaderedContentControl/samples/HeaderedContentControlSample.xaml.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Controls;
+
+namespace HeaderedContentControlExperiment.Samples;
+
+[ToolkitSample(id: nameof(HeaderedContentControlSample), "HeaderedContentControl", description: $"A sample for showing how to create and use a {nameof(HeaderedContentControl)} control.")]
+public sealed partial class HeaderedContentControlSample : Page
+{
+    public HeaderedContentControlSample()
+    {
+        this.InitializeComponent();
+    }
+}
