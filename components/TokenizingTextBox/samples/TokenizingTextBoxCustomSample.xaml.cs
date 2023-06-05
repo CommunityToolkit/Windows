44d9bd92380aeebc6911d804efd7816ce11c7ed2
Niels Laute <niels.laute@live.nl>
2023-06-05 10:57:15 +0200

    More changes

--- components/TokenizingTextBox/samples/TokenizingTextBoxCustomSample.xaml.cs
+++ components/TokenizingTextBox/samples/TokenizingTextBoxCustomSample.xaml.cs
@@ -3,11 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using CommunityToolkit.WinUI.Controls;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
 
 namespace TokenizingTextBoxExperiment.Samples;
 
