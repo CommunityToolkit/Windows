873ca65be6a9d09ef64bcdb444db77d245bead50
Niels Laute <niels.laute@live.nl>
2023-06-15 16:54:57 +0200

    Init

---
+++ components/ImageCropper/samples/ImageCropperCustomSample.xaml.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Controls;
+
+namespace ImageCropperExperiment.Samples;
+
+[ToolkitSampleMultiChoiceOption("ThumbPlacement", "All", "Corners", Title = "Thumb Placement")]
+[ToolkitSampleMultiChoiceOption("CropShape", "Rectangular", "Circular", Title = "Crop Shape")]
+
+[ToolkitSample(id: nameof(ImageCropperCustomSample), "ImageCropper", description: $"A sample for showing how to create and use a {nameof(ImageCropper)}.")]
+public sealed partial class ImageCropperCustomSample : Page
+{
+    public ImageCropperCustomSample()
+    {
+        this.InitializeComponent();
+    }
+
+    public static ThumbPlacement ConvertStringToThumbPlacement(string placement) => placement switch
+    {
+        "All" => ThumbPlacement.All,
+        "Corners" => ThumbPlacement.Corners,
+        _ => throw new System.NotImplementedException(),
+    };
+
+    public static CropShape ConvertStringToCropShape(string cropshape) => cropshape switch
+    {
+        "Circular" => CropShape.Circular,
+        "Rectangular" => CropShape.Rectangular,
+        _ => throw new System.NotImplementedException(),
+    };
+}
