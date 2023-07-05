f5d45f808316557418c17e88de4da6558e6b71af
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-07-05 14:24:53 -0500

    Initial port of CommunityToolkit.WinUI.Media to 8.x

---
+++ components/Media/samples/Media.md
@@ -0,0 +1,15 @@
+---
+title: Media
+author: githubaccount
+description: TODO: Your experiment's description here
+keywords: Media
+dev_langs:
+  - csharp
+category: Animations
+subcategory: Media
+discussion-id: 0
+issue-id: 0
+icon: assets/icon.png
+---
+
+TODO: Fill in information about this experiment and how to get started here...
---
+++ components/Media/src/Effects/Extensions/TileSourceExtension.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Media.Pipelines;
+
+namespace CommunityToolkit.WinUI.Media;
+
+/// <summary>
+/// An effect that loads an image and replicates it to cover all the available surface area
+/// </summary>
+/// <remarks>This effect maps to the Win2D <see cref="Graphics.Canvas.Effects.BorderEffect"/> effect</remarks>
+public sealed class TileSourceExtension : ImageSourceBaseExtension
+{
+    /// <inheritdoc/>
+    protected override object ProvideValue()
+    {
+        default(ArgumentNullException).ThrowIfNull(Uri);
+        return PipelineBuilder.FromTiles(Uri, DpiMode, CacheMode);
+    }
+}
