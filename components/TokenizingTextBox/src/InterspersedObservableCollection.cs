34e8ab60df1666caf6f355d3796fbfad41428e34
Niels Laute <niels.laute@live.nl>
2023-07-06 15:03:50 +0200

    Update components/TokenizingTextBox/src/InterspersedObservableCollection.cs
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>

--- components/TokenizingTextBox/src/InterspersedObservableCollection.cs
+++ components/TokenizingTextBox/src/InterspersedObservableCollection.cs
@@ -63,7 +63,7 @@ internal class InterspersedObservableCollection : IList, IEnumerable<object>, IN
         {
             var weakPropertyChangedListener = new WeakEventListener<InterspersedObservableCollection, object, NotifyCollectionChangedEventArgs>(this)
             {
-                OnEventAction = (instance, source, eventArgs) => instance.ItemsSource_CollectionChanged(source, eventArgs),
+                OnEventAction = static (instance, source, eventArgs) => instance.ItemsSource_CollectionChanged(source, eventArgs),
                 OnDetachAction = (weakEventListener) => notifier.CollectionChanged -= weakEventListener.OnEvent // Use Local Reference Only
             };
             notifier.CollectionChanged += weakPropertyChangedListener.OnEvent;
