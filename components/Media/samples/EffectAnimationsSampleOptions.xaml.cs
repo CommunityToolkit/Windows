e189866b532d94d1adb28e207a4a3db1237ee848
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-07-17 14:21:28 -0500

    Consolidate custom options panes

--- components/Media/samples/EffectAnimationsSampleOptions.xaml.cs
+++ components/Media/samples/EffectAnimationsSampleOptions.xaml.cs
@@ -2,26 +2,42 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CommunityToolkit.WinUI.Animations;
+
 namespace MediaExperiment.Samples;
 
 [ToolkitSampleOptionsPane(nameof(EffectAnimationsSample))]
+[ToolkitSampleOptionsPane(nameof(ColorEffectAnimationSample))]
+[ToolkitSampleOptionsPane(nameof(BlurEffectAnimationSample))]
 public sealed partial class EffectAnimationsSampleOptions : Page
 {
-    private EffectAnimationsSample _sampleInstance;
+    private AnimationSet _animationSet;
 
     public EffectAnimationsSampleOptions(EffectAnimationsSample sampleInstance)
     {
-        _sampleInstance = sampleInstance;
         this.InitializeComponent();
+        _animationSet = new EffectAnimationsSample.XamlNamedPropertyRelay(sampleInstance).ClipAnimation;
+    }
+
+    public EffectAnimationsSampleOptions(ColorEffectAnimationSample sampleInstance)
+    {
+        this.InitializeComponent();
+        _animationSet = new ColorEffectAnimationSample.XamlNamedPropertyRelay(sampleInstance).ColorAnimation;
+    }
+
+    public EffectAnimationsSampleOptions(BlurEffectAnimationSample sampleInstance)
+    {
+        this.InitializeComponent();
+        _animationSet = new BlurEffectAnimationSample.XamlNamedPropertyRelay(sampleInstance).BlurAnimation;
     }
 
     public void OnStartAnimationButton_Clicked(object sender, RoutedEventArgs e)
     {
-        new EffectAnimationsSample.XamlNamedPropertyRelay(_sampleInstance).ClipAnimation.Start();
+        _animationSet.Start();
     }
 
     public void OnStopAnimationButton_Clicked(object sender, RoutedEventArgs e)
     {
-        new EffectAnimationsSample.XamlNamedPropertyRelay(_sampleInstance).ClipAnimation.Stop();
+        _animationSet.Stop();
     }
 }
