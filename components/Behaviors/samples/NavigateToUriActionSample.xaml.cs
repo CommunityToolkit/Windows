d3a1be77dbc705b40e7dc87bb226f71b621ef251
Niels Laute <niels.laute@live.nl>
2023-08-22 18:11:13 +0200

    Porting NavigateToUriBehavior (#179)
    * Adding NavigateToUriBehavior
    
    * Address feedback
    
    * Comment tweak

---
+++ components/Behaviors/samples/NavigateToUriActionSample.xaml.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.Behaviors;
+
+namespace BehaviorsExperiment.Samples;
+
+[ToolkitSample(id: nameof(NavigateToUriActionSample), nameof(NavigateToUriAction), description: $"A sample demonstrating how to use {nameof(NavigateToUriAction)}.")]
+public sealed partial class NavigateToUriActionSample : Page
+{
+    public NavigateToUriActionSample()
+    {
+        this.InitializeComponent();
+    }
+}
