769455182aa0bcee667a70432d1644930c090e69
Niels Laute <niels.laute@live.nl>
2023-08-24 14:41:57 +0200

    Removing redundant namespaces

--- components/Primitives/src/WrapLayout/UvMeasure.cs
+++ components/Primitives/src/WrapLayout/UvMeasure.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
-using Windows.Foundation;
-using Microsoft.UI.Xaml.Controls;
-
 namespace CommunityToolkit.WinUI.Controls;
 
 [DebuggerDisplay("U = {U} V = {V}")]
--- components/Primitives/src/WrapLayout/WrapLayout.cs
+++ components/Primitives/src/WrapLayout/WrapLayout.cs
@@ -2,11 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using Microsoft.UI.Xaml.Controls;
 using System.Collections.Specialized;
-using Windows.Foundation;
-using Microsoft.UI.Xaml;
 
 namespace CommunityToolkit.WinUI.Controls;
 
