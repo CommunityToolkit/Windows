0931d92d185119c937b1b9096c28b4e8243d7957
Niels Laute <niels.laute@live.nl>
2023-09-07 10:20:10 +0200

    Address feedback

--- components/Collections/tests/Test_IncrementalLoadingCollection.cs
+++ components/Collections/tests/Test_IncrementalLoadingCollection.cs
@@ -2,8 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace CollectionsExperiment.Tests;
 using CommunityToolkit.WinUI.Collections;
+
+namespace CollectionsExperiment.Tests;
 
 [TestClass]
 public class Test_IncrementalLoadingCollection
