8736642c5e207c0cf5f0eaa3dbf6a16ca610de40
Birler <daniel.birler@raedlinger.com>
2023-10-05 16:49:51 +0200

    Clean up suppression of CS0114

--- components/LayoutTransformControl/src/LayoutTransformControl.Properties.cs
+++ components/LayoutTransformControl/src/LayoutTransformControl.Properties.cs
@@ -69,9 +69,7 @@ public partial class LayoutTransformControl : Control
     /// <remarks>
     /// Corresponds to UIElement.RenderTransform.
     /// </remarks>
-#pragma warning disable CS0114 // Member does not hide an inherited member; new keyword is not required
     public Transform Transform
-#pragma warning restore CS0114 // Member does not hide an inherited member; new keyword is not required
     {
         get { return (Transform)GetValue(TransformProperty); }
         set { SetValue(TransformProperty, value); }
