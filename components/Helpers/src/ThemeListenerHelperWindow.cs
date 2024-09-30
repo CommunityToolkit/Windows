9287da42cb613dd1ebe89558bba24d1d593ab572
DismissedLight <1686188646@qq.com>
2024-09-30 10:28:40 +0800

    code style

--- components/Helpers/src/ThemeListenerHelperWindow.cs
+++ components/Helpers/src/ThemeListenerHelperWindow.cs
@@ -3,90 +3,77 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace CommunityToolkit.WinUI.HelpersRns;
 
 #if WINAPPSDK
-
 internal class ThemeListenerHelperWindow
 {
     public delegate void ThemeChangedHandler(ApplicationTheme theme);
 
     public static ThemeListenerHelperWindow Instance = new();
-    private static string s_className = "ThemeListenerHelperWindow";
-    private IntPtr m_hwnd;
+
     public event ThemeChangedHandler? ThemeChanged;
-    delegate Windows.Win32.Foundation.LRESULT WndProc(Windows.Win32.Foundation.HWND hWnd, uint msg, Windows.Win32.Foundation.WPARAM wParam, Windows.Win32.Foundation.LPARAM lParam);
-    private WndProc m_wnd_proc_delegate;
-    private void registerClass()
-    {
-        unsafe
-        {
-            var wcx = new Windows.Win32.UI.WindowsAndMessaging.WNDCLASSEXW();
-            wcx.cbSize = (uint)Marshal.SizeOf(wcx);
-            fixed (char* pClassName = s_className)
-            {
-                wcx.lpszClassName = new Windows.Win32.Foundation.PCWSTR(pClassName);
-            }
-            wcx.lpfnWndProc = wndProc;
-            if (PInvoke.RegisterClassEx(in wcx) == 0)
-            {
-                Debug.WriteLine(new Win32Exception(Marshal.GetLastWin32Error()).Message);
-            }
-        }
 
+    private static string s_className = "ThemeListenerHelperWindow";
+
+    private HWND m_hwnd;
+
+    private ThemeListenerHelperWindow()
+    {
+        s_className = "ThemeListenerHelperWindow";
+        RegisterClass();
+        m_hwnd = CreateWindow();
     }
 
-    static Windows.Win32.Foundation.LRESULT wndProc(Windows.Win32.Foundation.HWND hWnd, uint msg, Windows.Win32.Foundation.WPARAM wParam, Windows.Win32.Foundation.LPARAM lParam)
+    private static unsafe void RegisterClass()
     {
-        switch (msg)
+        WNDCLASSEXW wcx = default;
+        wcx.cbSize = (uint)sizeof(WNDCLASSEXW);
+        fixed (char* pClassName = s_className)
         {
-            case PInvoke.WM_SETTINGCHANGE:
-                var value = Registry.GetValue(
-                    """HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize""",
-                    "AppsUseLightTheme",
-                    true
-                    );
-                if (value != null)
-                {
-                    Instance.ThemeChanged?.Invoke((int)value == 1 ? ApplicationTheme.Light : ApplicationTheme.Dark);
-                }
-                break;
+            wcx.lpszClassName = pClassName;
+            wcx.lpfnWndProc = &WndProc;
+            if (PInvoke.RegisterClassEx(in wcx) is 0)
+            {
+                Marshal.ThrowExceptionForHR(Marshal.GetLastPInvokeError());
+            }
         }
-        return PInvoke.DefWindowProc(hWnd, msg, wParam, lParam);
     }
 
-    unsafe private static IntPtr createWindow()
+    private static unsafe HWND CreateWindow()
     {
-        var hwnd = PInvoke.CreateWindowEx(
-            0,
-            s_className,
-            "",
-            0,
-            0, 0, 0, 0,
-            new Windows.Win32.Foundation.HWND(),
-            null,
-            null,
-            null);
-        if (hwnd == IntPtr.Zero)
+        HWND hwnd = PInvoke.CreateWindowEx(0, s_className, string.Empty, 0, 0, 0, 0, 0, default, null, null, null);
+        if (hwnd == HWND.Null)
         {
-            Debug.WriteLine(new Win32Exception(Marshal.GetLastWin32Error()).Message);
+            Marshal.ThrowExceptionForHR(Marshal.GetLastPInvokeError());
         }
         return hwnd;
     }
-    private ThemeListenerHelperWindow()
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)])]
+    private static LRESULT WndProc(HWND hWnd, uint msg, WPARAM wParam, LPARAM lParam)
     {
-        m_wnd_proc_delegate = wndProc;
-        s_className = "ThemeListenerHelperWindow";
-        registerClass();
-        m_hwnd = createWindow();
+        if (msg is PInvoke.WM_SETTINGCHANGE)
+        {
+            // REG_DWORD
+            object? value = Registry.GetValue(
+                @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+                "AppsUseLightTheme",
+                true);
+
+            if (value is int dword)
+            {
+                Instance.ThemeChanged?.Invoke(dword is 1 ? ApplicationTheme.Light : ApplicationTheme.Dark);
+            }
+        }
+
+        return PInvoke.DefWindowProc(hWnd, msg, wParam, lParam);
     }
 }
 #endif
