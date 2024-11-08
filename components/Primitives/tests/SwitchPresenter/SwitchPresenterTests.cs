cd19f5580dc58e3948e323662913cd6a19d3fe8b
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-11-08 09:07:25 -0800

    [Test] Add SwitchConverter tests to the primitives component
    Fix test header (failure in CI from last commit)

--- components/Primitives/tests/SwitchPresenter/SwitchPresenterTests.cs
+++ components/Primitives/tests/SwitchPresenter/SwitchPresenterTests.cs
@@ -1,6 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using CommunityToolkit.Tests;
 using CommunityToolkit.Tooling.TestGen;
 using CommunityToolkit.WinUI.Controls;
+using CommunityToolkit.WinUI.Converters;
 
 namespace PrimitivesExperiment.Tests;
 
@@ -47,5 +52,91 @@ public partial class SwitchPresenterTests : VisualUITestBase
         var txtbox = spresenter.FindDescendant<TextBox>();
         Assert.IsNotNull(txtbox, "Couldn't find new textbox");
         Assert.AreEqual("Confirmation code", txtbox.Header, "Textbox header not expected value");
+    }
+
+    [UIThreadTestMethod]
+    public async Task SwitchConverterBrushTest(SwitchConverterBrushSample page)
+    {
+        var combobox = page.FindDescendant<ComboBox>();
+        var textblock = page.FindDescendant("ResultBlock") as TextBlock;
+
+        Assert.IsNotNull(combobox, "Couldn't find ComboBox");
+        Assert.IsNotNull(textblock, "Couldn't find SwitchPresenter");
+
+        // Are we in our initial case?
+        Assert.AreEqual(0, combobox.SelectedIndex, "ComboBox not initialized");
+        Assert.AreEqual("Success", combobox.SelectedItem, "Not expected starting value in ComboBox");
+
+        // Check the TextBlock's brush
+        Assert.AreEqual(((SolidColorBrush)page.Resources["SystemFillColorSuccessBrush"]).Color, ((SolidColorBrush)textblock.Foreground).Color, "TextBlock not in initial success state");
+
+        // Update combobox
+        combobox.SelectedIndex = 1;
+
+        // Wait for update
+        await CompositionTargetHelper.ExecuteAfterCompositionRenderingAsync(() => { });
+
+        // Are we in the new case?
+        Assert.AreEqual("Warning", combobox.SelectedItem, "ComboBox didn't change");
+
+        // Check the TextBlock's brush
+        Assert.AreEqual(((SolidColorBrush)page.Resources["SystemFillColorCautionBrush"]).Color, ((SolidColorBrush)textblock.Foreground).Color, "TextBlock not in new state");
+
+        // Update combobox
+        combobox.SelectedIndex = 2;
+
+        // Wait for update
+        await CompositionTargetHelper.ExecuteAfterCompositionRenderingAsync(() => { });
+
+        // Are we in the new case?
+        Assert.AreEqual("Error", combobox.SelectedItem, "ComboBox didn't change 2");
+
+        // Check the TextBlock's brush
+        Assert.AreEqual(((SolidColorBrush)page.Resources["SystemFillColorCriticalBrush"]).Color, ((SolidColorBrush)textblock.Foreground).Color, "TextBlock not in final state");
+    }
+
+    [UIThreadTestMethod]
+    public void SwitchConverterDirectTest()
+    {
+        // Multiply by 10
+        SwitchConverter sconverter = new()
+        {
+            SwitchCases = new CaseCollection {
+                new Case()
+                {
+                    Content = 10,
+                    Value = 1,
+                },
+                new Case()
+                {
+                    Content = 50,
+                    Value = 5,
+                    IsDefault = true
+                },
+                new Case()
+                {
+                    Content = 30,
+                    Value = 3,
+                },
+                new Case()
+                {
+                    Content = 20,
+                    Value = 2,
+                },
+            },
+            TargetType = typeof(int)
+        };
+
+        Assert.IsNotNull(sconverter);
+        Assert.AreEqual(4, sconverter.SwitchCases.Count, "Not 4 cases");
+
+        var @default = sconverter.Convert(100, typeof(int), string.Empty, string.Empty);
+
+        Assert.AreEqual(50, @default, "Unexpected default return");
+
+        Assert.AreEqual(10, sconverter.Convert(1, typeof(int), string.Empty, string.Empty), "Unexpected result with 1");
+        Assert.AreEqual(20, sconverter.Convert(2, typeof(int), string.Empty, string.Empty), "Unexpected result with 2");
+        Assert.AreEqual(30, sconverter.Convert(3, typeof(int), string.Empty, string.Empty), "Unexpected result with 3");
+        Assert.AreEqual(50, sconverter.Convert(5, typeof(int), string.Empty, string.Empty), "Unexpected result with 5");
     }
 }
