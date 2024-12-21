94576a93765fc78d38d639047bc3f6e86f4b833d
Arlo <arlo.godfrey@outlook.com>
2024-12-20 18:26:19 -0600

    Add XML documentation to NullToTransparentConverter class

--- components/ColorPicker/src/Converters/NullToTransparentConverter.cs
+++ components/ColorPicker/src/Converters/NullToTransparentConverter.cs
@@ -4,10 +4,15 @@
 
 namespace CommunityToolkit.WinUI.Controls;
 
+/// <summary>
+/// Value converter that converts null values to Transparent.
+/// </summary>
 public partial class NullToTransparentConverter : IValueConverter
 {
+    /// <inheritdoc/>
     public object Convert(object value, Type targetType, object parameter, string language) => value;
-
+    
+    /// <inheritdoc/>
     public object ConvertBack(object? value, Type targetType, object parameter, string language) => value ??
 #if WINUI2
         Windows.UI.Colors.Transparent;
