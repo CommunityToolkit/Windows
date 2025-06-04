ba96a3da4ee766e48d33d1d50a9ba6e40b423af8
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-06-04 23:08:57 +0900

    Init

---
+++ components/BladeView/src/BladeView_xBind.cs
@@ -0,0 +1,71 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CommunityToolkit.WinUI.Controls;
+
+/// <summary>
+/// An example templated control.
+/// </summary>
+public partial class BladeView_xBind: Control
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="BladeView_xBind"/> class.
+    /// </summary>
+    public BladeView_xBind()
+    {
+        this.DefaultStyleKey = typeof(BladeView_xBind);
+
+        // Allows directly using this control as the x:DataType in the template.
+        this.DataContext = this;
+    }
+
+    /// <summary>
+    /// The backing <see cref="DependencyProperty"/> for the <see cref="ItemPadding"/> property.
+    /// </summary>
+    public static readonly DependencyProperty ItemPaddingProperty = DependencyProperty.Register(
+        nameof(ItemPadding),
+        typeof(Thickness),
+        typeof(BladeView_xBind),
+        new PropertyMetadata(defaultValue: new Thickness(0)));
+
+    /// <summary>
+    /// The backing <see cref="DependencyProperty"/> for the <see cref="MyProperty"/> property.
+    /// </summary>
+    public static readonly DependencyProperty MyPropertyProperty = DependencyProperty.Register(
+        nameof(MyProperty),
+        typeof(string),
+        typeof(BladeView_xBind),
+        new PropertyMetadata(defaultValue: string.Empty, (d, e) => ((BladeView_xBind)d).OnMyPropertyChanged((string)e.OldValue, (string)e.NewValue)));
+
+    /// <summary>
+    /// Gets or sets an example string. A basic DependencyProperty example.
+    /// </summary>
+    public string MyProperty
+    {
+        get => (string)GetValue(MyPropertyProperty);
+        set => SetValue(MyPropertyProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets a padding for an item. A basic DependencyProperty example.
+    /// </summary>
+    public Thickness ItemPadding
+    {
+        get => (Thickness)GetValue(ItemPaddingProperty);
+        set => SetValue(ItemPaddingProperty, value);
+    }
+
+    protected virtual void OnMyPropertyChanged(string oldValue, string newValue)
+    {
+        // Do something with the changed value.
+    }
+
+    public void Element_PointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        if (sender is TextBlock text)
+        {
+            text.Opacity = 1;
+        }
+    }
+}
