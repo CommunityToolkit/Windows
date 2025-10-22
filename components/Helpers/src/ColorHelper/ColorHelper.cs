2bdfcd24c2112cb57bca4347505e71604dea2207
Adam Dernis <Avid29@Live.com>
2025-10-22 07:08:09 +0300

    Split ParseColor into various child methods for different formats

--- components/Helpers/src/ColorHelper/ColorHelper.cs
+++ components/Helpers/src/ColorHelper/ColorHelper.cs
@@ -19,6 +19,165 @@ namespace CommunityToolkit.WinUI.Helpers;
 public static partial class ColorHelper
 {
     /// <summary>
+    /// Attempts to parse a string as a color.
+    /// </summary>
+    /// <remarks>
+    /// Any format used in XAML should work.
+    /// </remarks>
+    /// <param name="colorString">The string to parse.</param>
+    /// <param name="color">The resulting color.</param>
+    /// <returns>Whether or not the parse succeeded.</returns>
+    public static bool TryParseColor(string colorString, out Color color)
+    {
+        color = default;
+
+        if (string.IsNullOrEmpty(colorString))
+            return false;
+
+        // Try as hex
+        if (TryParseHexColor(colorString, out color))
+            return true;
+
+        // Try as screen color
+        if (TryParseScreenColor(colorString, out color))
+            return true;
+
+        if (TryParseColorName(colorString, out color))
+            return true;
+
+        return false;
+    }
+
+    /// <summary>
+    /// Attempts to parse a string as a hex color.
+    /// </summary>
+    /// <param name="hexString">The string to parse.</param>
+    /// <param name="color">The resulting color.</param>
+    /// <returns>Whether or not the parse succeeded.</returns>
+    public static bool TryParseHexColor(string hexString, out Color color)
+    {
+        color = default;
+
+        // Ensure string begins with '#'
+        // and is long enough to not break later in the parser.
+        if (string.IsNullOrEmpty(hexString) ||
+            hexString.Length < 2 ||
+            hexString[0] != '#')
+            return false;
+
+        // Convert base 16 string to uint
+        if(!uint.TryParse(hexString[1..], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var cuint))
+            return false;
+
+        // Extract 4 bytes
+        byte b3 = (byte)(cuint >> 24);
+        byte b2 = (byte)((cuint >> 16) & 0xff);
+        byte b1 = (byte)((cuint >> 8) & 0xff);
+        byte b0 = (byte)(cuint & 0xff);
+
+        // Extract 4 half-bytes
+        byte h3 = (byte)(cuint >> 12);
+        byte h2 = (byte)((cuint >> 8) & 0xf);
+        byte h1 = (byte)((cuint >> 4) & 0xf);
+        byte h0 = (byte)(cuint & 0xf);
+        h3 = (byte)(h3 << 4 | h3);
+        h2 = (byte)(h2 << 4 | h2);
+        h1 = (byte)(h1 << 4 | h1);
+        h0 = (byte)(h0 << 4 | h0);
+
+        // Select bytes based on string length
+        switch (hexString.Length)
+        {
+            // #AaRrGgBb
+            case 9:
+                color = Color.FromArgb(b3, b2, b1, b0);
+                return true;
+            // #RrGgBb
+            case 7:
+                color = Color.FromArgb(255, b2, b1, b0);
+                return true;
+            // #ARGB
+            case 5:
+                color = Color.FromArgb(h3, h2, h1, h0);
+                return true;
+            // #RGB
+            case 4:
+                color = Color.FromArgb(255, h2, h1, h0);
+                return true;
+            // Invalid format
+            default:
+                return false;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to parse a string as a screen color.
+    /// </summary>
+    /// <param name="screenColor">The string to parse.</param>
+    /// <param name="color">The resulting color.</param>
+    /// <returns>Whether or not the parse succeeded.</returns>
+    public static bool TryParseScreenColor(string screenColor, out Color color)
+    {
+        color = default;
+
+        // Ensure the string begins with "sc#"
+        if (screenColor.Length < 3 || screenColor[0..3] != "sc#")
+            return false;
+
+        // Get arguments
+        screenColor = screenColor[3..];
+        var values = screenColor.Split(',');
+
+        // Parse the arguments from string doubles into bytes
+        var args = new byte[values.Length];
+        for (int i = 0; i <  values.Length; i++)
+        {
+            if (!double.TryParse(values[i], out var arg))
+                return false;
+
+            args[i] = (byte)(arg * 255);
+        }
+
+        // Assign channel data based on arguments
+        switch (args.Length)
+        {
+            // sc#Alpha,Red,Green,Blue
+            case 4:
+                color = Color.FromArgb(args[0], args[1], args[2], args[3]);
+                return true;
+            // sc#Red,Green,Blue
+            case 3:
+                color = Color.FromArgb(255, args[0], args[1], args[2]);
+                return true;
+            // Invalid format
+            default:
+                return false;
+        }
+    }
+    
+    /// <summary>
+    /// Attempts to parse a string color name.
+    /// </summary>
+    /// <param name="colorName">The color name.</param>
+    /// <param name="color">The resulting color.</param>
+    /// <returns>Whether or not the parse succeeded.</returns>
+    public static bool TryParseColorName(string colorName, out Color color)
+    {
+        color = default;
+        
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+        var prop = typeof(Colors).GetTypeInfo().GetDeclaredProperty(colorName);
+        if (prop != null)
+        {
+            color = (Color)prop.GetValue(null);
+            return true;
+        }
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+
+        return false;
+    }
+
+    /// <summary>
     /// Creates a <see cref="Color"/> from a XAML color string.
     /// Any format used in XAML should work.
     /// </summary>
@@ -26,83 +185,50 @@ public static partial class ColorHelper
     /// <returns>The created <see cref="Color"/>.</returns>
     public static Color ParseColor(string colorString)
     {
-        if (string.IsNullOrEmpty(colorString))
-        {
-            ThrowArgumentException();
-        }
+        if (TryParseColor(colorString, out var color))
+            return color;
 
-        if (colorString[0] == '#')
-        {
-            var cuint = Convert.ToUInt32(colorString[1..], 16);
+        throw new FormatException($"The string '{colorString}' could not be parsed as a string.");
+    }
 
-            // 4 bytes
-            byte b3 = (byte)(cuint >> 24);
-            byte b2 = (byte)((cuint >> 16) & 0xff);
-            byte b1 = (byte)((cuint >> 8) & 0xff);
-            byte b0 = (byte)(cuint & 0xff);
+    /// <summary>
+    /// Parses a hexadecimal color string into a <see cref="Color"/>.
+    /// </summary>
+    /// <param name="hexColor">The hex color string.</param>
+    /// <returns>The resulting <see cref="Color"/>.</returns>
+    public static Color ParseHexColor(string hexColor)
+    {
+        if (TryParseHexColor(hexColor, out var color))
+            return color;
 
-            // 4 half-bytes
-            byte h3 = (byte)(cuint >> 12);
-            byte h2 = (byte)((cuint >> 8) & 0xf);
-            byte h1 = (byte)((cuint >> 4) & 0xf);
-            byte h0 = (byte)(cuint & 0xf);
-            h3 = (byte)(h3 << 4 | h3);
-            h2 = (byte)(h2 << 4 | h2);
-            h1 = (byte)(h1 << 4 | h1);
-            h0 = (byte)(h0 << 4 | h0);
+        throw new FormatException($"The string '{hexColor}' could not be parsed as a hex color.");
+    }
 
-            byte r, g, b, a;
-            (a, r, g, b) = colorString.Length switch
-            {
-                9 => (b3, b2, b1, b0),
-                7 => ((byte)255, b2, b1, b0),
-                5 => (h3, h2, h1, h0),
-                4 => ((byte)255, h2, h1, h0),
-                _ => ThrowFormatException<(byte, byte, byte, byte)>(),
-            };
+    /// <summary>
+    /// Parses a screen color string into a <see cref="Color"/>.
+    /// </summary>
+    /// <param name="screenColor">The screen color string.</param>
+    /// <returns>The resulting <see cref="Color"/>.</returns>
+    public static Color ParseScreenColor(string screenColor)
+    {
+        if (TryParseScreenColor(screenColor, out var color))
+            return color;
 
-            return Color.FromArgb(a, r, g, b);
-        }
+        throw new FormatException($"The string '{screenColor}' is not a valid ScreenColor string");
+    }
 
-        if (colorString.Length > 3 && colorString[0] == 's' && colorString[1] == 'c' && colorString[2] == '#')
-        {
-            var values = colorString.Split(',');
+    /// <summary>
+    /// Parses a color by name.
+    /// </summary>
+    /// <param name="colorName">The color's name.</param>
+    /// <returns>The resulting <see cref="Color"/>.</returns>
+    /// <exception cref="ArgumentException">Throws if the color name is not recognized.</exception>
+    public static Color ParseColorName(string colorName)
+    {
+        if (TryParseColorName(colorName, out var color))
+            return color;
 
-            if (values.Length == 4)
-            {
-                var scA = double.Parse(values[0].Substring(3), CultureInfo.InvariantCulture);
-                var scR = double.Parse(values[1], CultureInfo.InvariantCulture);
-                var scG = double.Parse(values[2], CultureInfo.InvariantCulture);
-                var scB = double.Parse(values[3], CultureInfo.InvariantCulture);
-
-                return Color.FromArgb((byte)(scA * 255), (byte)(scR * 255), (byte)(scG * 255), (byte)(scB * 255));
-            }
-
-            if (values.Length == 3)
-            {
-                var scR = double.Parse(values[0].Substring(3), CultureInfo.InvariantCulture);
-                var scG = double.Parse(values[1], CultureInfo.InvariantCulture);
-                var scB = double.Parse(values[2], CultureInfo.InvariantCulture);
-
-                return Color.FromArgb(255, (byte)(scR * 255), (byte)(scG * 255), (byte)(scB * 255));
-            }
-
-            return ThrowFormatException<Color>();
-        }
-
-        var prop = typeof(Colors).GetTypeInfo().GetDeclaredProperty(colorString);
-
-        if (prop != null)
-        {
-#pragma warning disable CS8605 // Unboxing a possibly null value.
-            return (Color)prop.GetValue(null);
-#pragma warning restore CS8605 // Unboxing a possibly null value.
-        }
-
-        return ThrowFormatException<Color>();
-
-        static void ThrowArgumentException() => throw new ArgumentException("The parameter \"colorString\" must not be null or empty.");
-        static T ThrowFormatException<T>() => throw new FormatException("The parameter \"colorString\" is not a recognized Color format.");
+        throw new FormatException($"The name '{colorName}' not a valid color");
     }
 
     internal static (double h1, double chroma) CalculateHueAndChroma(Color color, out double min, out double max, out double alpha)
@@ -114,7 +240,7 @@ public static partial class ColorHelper
         var g = (double)color.G / 255;
         var b = (double)color.B / 255;
         alpha = (double)color.A / 255;
-        
+
         max = Math.Max(Math.Max(r, g), b);
         min = Math.Min(Math.Min(r, g), b);
         var chroma = max - min;
@@ -129,7 +255,8 @@ public static partial class ColorHelper
         {
             // Red is max
             h1 = ((g - b) / chroma + 6) % 6;
-        } else if (max == g)
+        }
+        else if (max == g)
         {
             // Green is max
             h1 = 2 + ((b - r) / chroma);
@@ -152,7 +279,7 @@ public static partial class ColorHelper
         (r1, g1, b1) = h1 switch
         {
             < 1 => (chroma, x, 0d),
-            < 2 => (x,  chroma, 0d),
+            < 2 => (x, chroma, 0d),
             < 3 => (0d, chroma, x),
             < 4 => (0d, x, chroma),
             < 5 => (x, 0d, chroma),
